fef2d6541a643ee8b37ffa3c7c4d1fe4f75583ca
Philip Kwesiga <phil.kwesiga@gmail.com>
2018-06-04 00:51:13 +0300

    add front end target group

--- target_groups.tf
+++ target_groups.tf
@@ -14,3 +14,15 @@ resource "aws_alb_target_group_attachment" "internal_target" {
     target_group_arn = "${aws_alb_target_group.internal_target_group.arn}"
     target_id        = "${aws_instance.demo_backend_terraform.id}"
 }
+
+resource "aws_alb_target_group" "frontend_target_group" {
+    name = "frontend-target-group"
+    port = 80
+    protocol = "HTTP"
+    vpc_id = "${aws_vpc.demo_vpc_terraform.id}"
+    health_check{
+        health_threshold = 5
+        unhealthy_threshold = 2
+        timeout = 5
+    }
+}
