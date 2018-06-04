203ed5d8e0eb2a91d6c7ae01d3bcf5f9250b3413
Philip Kwesiga <phil.kwesiga@gmail.com>
2018-06-04 05:28:25 +0300

    edit launch configuration security group

--- launch_configuration.tf
+++ launch_configuration.tf
@@ -1,7 +1,7 @@
 resource "aws_launch_configuration" "demo_launch_conf" {
     image_id= "${var.frontend_ami}"
-    instance_type = “t2.micro”
-    security_groups = ["${aws_security_group.demo_frontend_sg.id}"]
+    instance_type = "t2.micro"
+    security_groups = ["${aws_security_group.demo_frontend_alb_sg.id}"]
     key_name = "${var.key_name}"
     lifecycle {
         create_before_destroy = true
