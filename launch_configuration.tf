72f5444017494541fa86bf343845212613222839
Philip Kwesiga <phil.kwesiga@gmail.com>
2018-06-05 03:17:18 +0300

    Add image ami

--- launch_configuration.tf
+++ launch_configuration.tf
@@ -1,5 +1,6 @@
 resource "aws_launch_configuration" "demo_launch_conf" {
-    image_id= "${var.frontend_ami}"
+    name = "demo-launch-conf"
+    image_id = "${var.frontend_ami}"
     instance_type = "t2.micro"
     security_groups = ["${aws_security_group.demo_frontend_alb_sg.id}"]
     key_name = "${var.key_name}"
